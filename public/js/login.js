686c1f119399c7242f5d275052c4af9314b6e162
mxh77 <maxime.heron@gmail.com>
2024-12-21 09:13:12 +0100

    Update deployment files

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
+import config from './config.js';
+
 console.log('login.js loaded');
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -15,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
 
-            fetch('/auth/login', {
+            fetch(`${config.backendUrl}/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -54,35 +56,35 @@ document.addEventListener('DOMContentLoaded', function () {
             const email = forgotEmail.value;
             console.log('Email entered:', email);
 
-            fetch('/auth/forgot-password', {
-                method: 'POST',
+            fetch(`${config.backendUrl}/auth/forgot-password`, {
+            method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email })
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email })
+        })
+            .then(response => {
+                console.log('Response status:', response.status);
+                return response.json().then(data => ({ status: response.status, body: data }));
             })
-                .then(response => {
-                    console.log('Response status:', response.status);
-                    return response.json().then(data => ({ status: response.status, body: data }));
-                })
-                .then(({ status, body }) => {
-                    console.log('Response body:', body);
-                    forgotPasswordMessage.style.display = 'block';
-                    if (status === 200) {
-                        forgotPasswordMessage.textContent = 'Le mdail de réinitialisation de votre mot de passe a été envoyé.';
-                        forgotPasswordMessage.className = 'text-success mt-3';
-                    } else {
-                        forgotPasswordMessage.textContent = body.msg || 'Une erreur est survenue.';
-                        forgotPasswordMessage.className = 'text-danger mt-3';
-                        console.error('Forgot password failed:', body.msg);
-                    }
-                })
-                .catch(error => {
-                    console.error('Fetch error:', error);
-                    forgotPasswordMessage.style.display = 'block';
-                    forgotPasswordMessage.textContent = 'Une erreur est survenue. Veuillez réessayer.';
+            .then(({ status, body }) => {
+                console.log('Response body:', body);
+                forgotPasswordMessage.style.display = 'block';
+                if (status === 200) {
+                    forgotPasswordMessage.textContent = 'Le mdail de réinitialisation de votre mot de passe a été envoyé.';
+                    forgotPasswordMessage.className = 'text-success mt-3';
+                } else {
+                    forgotPasswordMessage.textContent = body.msg || 'Une erreur est survenue.';
                     forgotPasswordMessage.className = 'text-danger mt-3';
-                });
-        });
+                    console.error('Forgot password failed:', body.msg);
+                }
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+                forgotPasswordMessage.style.display = 'block';
+                forgotPasswordMessage.textContent = 'Une erreur est survenue. Veuillez réessayer.';
+                forgotPasswordMessage.className = 'text-danger mt-3';
+            });
+    });
     }
 });
